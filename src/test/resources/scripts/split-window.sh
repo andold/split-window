0d2b31348a469c5dde0404d44a034baffaf91615
andold <andold@naver.com>
2024-11-16 13:16:04 +0900

    move source directory

--- src/test/resources/scripts/split-window.sh
+++ src/test/resources/scripts/split-window.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
 #
+HOME_DIR=/home/andold
+SOURCE_DIR=$HOME_DIR/src/eclipse-workspace/split-window
+#
+#
+#
 if [ ! -d "./build" ]; then
 	mkdir ./build
 fi
@@ -9,11 +14,13 @@ if [ -f "./build" ]; then
 fi
 #
 #
+#
+cd $SOURCE_DIR
 g++ -o ./build/split-window ./src/main/cpp/split-window.cpp -L/usr/X11/lib -lX11 -lstdc++
 #
 #
-./build/split-window left
-./build/split-window left
-./build/split-window right
-
-/home/andold/eclipse-workspace/split-window/build/split-window right
+#
+$SOURCE_DIR/build/split-window left
+$SOURCE_DIR/build/split-window left
+$SOURCE_DIR/build/split-window right
+$SOURCE_DIR/build/split-window right
